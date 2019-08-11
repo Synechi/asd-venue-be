3fe64f919b5f596a91f468ff7da2495074793d28
Evan Hunter <evan.hunter@student.uts.edu.au>
2019-08-11 20:41:01 +1000

    Test heroku deploy failure

--- server.js
+++ server.js
@@ -38,6 +38,8 @@ router.route("/user").get((req, res) => {
   });
 });
 
+app.get("/", (req, res) => res.send("Hello World!"));
+
 app.use("/", router);
 
 app.listen(port, () => console.log("Express sever running on port " + port));
