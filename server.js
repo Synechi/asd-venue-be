e0217f877faa5b64b237a38c185685e3b73e1ece
root <root@DESKTOP-M4FKMLB.localdomain>
2019-09-01 15:27:10 +1000

    Display Pending Friend Requests!

--- server.js
+++ server.js
@@ -89,49 +89,156 @@ function search(id, array) {
 }
 
 //Updates 2 user documents in the DB with the sent friend request
-router.route("/friendRequest").post((req, res, next) => {
+router.route("/friendRequest/:friendID").post((req, res, next) => {
   //Hardcoding the userID for testing purposes because login feature is not established - please replace with req.session._id
   //Use that userID to to find its JSON file in the DB
-  
-    User.findById("5d626b53fa8afa89b5f13ebd", (err, user) => {
-      if (err) console.log(err);
-      else {
-        if (
-          search(req.body.friendID, user["USER_FRIENDS"]) === req.body.friendID
-        ) {
-          next(new Error("FRIEND_ALREADY_EXISTS"));
-        } else {
-          user["USER_FRIENDS"].push({
-            friendID: req.body.friendID,
-            friendStatus: "Sent"
-          }); //update the document of the user in session
 
-          user.save((err, user) => {
-            if (err) next(err);
-            User.findById(req.body.friendID, (err, friend) => {
-              if (err) console.log(err);
-              else {
-                friend["USER_FRIENDS"].push({
-                  friendID: "5d626b53fa8afa89b5f13ebd",
-                  friendStatus: "Received"
-                }); //update the document of the friend the request is being sent to
-              }
-              friend.save((err, friend) => {
-                if (err) next(err);
-                res.json(user);
-              });
+  User.findById("5d692742618d9401994782c3", (err, user) => {
+    if (err) console.log(err);
+    else {
+      if (
+        search(req.params.friendID, user["USER_FRIENDS"]) === req.params.friendID
+      ) {
+        next(new Error("FRIEND_ALREADY_EXISTS"));
+      } else {
+        user["USER_FRIENDS"].push({
+          friendID: req.params.friendID,
+          friendStatus: "Sent"
+        }); //update the document of the user in session
+
+        user.save((err, user) => {
+          if (err) next(err);
+          User.findById(req.params.friendID, (err, friend) => {
+            if (err) console.log(err);
+            else {
+              friend["USER_FRIENDS"].push({
+                friendID: "5d626b53fa8afa89b5f13ebd",
+                friendStatus: "Received"
+              }); //update the document of the friend the request is being sent to
+            }
+            friend.save((err, friend) => {
+              if (err) next(err);
+              res.json(user);
             });
           });
-        }
+        });
       }
-    });
-  
+    }
+  });
 });
 
-//Default Error-Handler: 
-app.use(function(error, req, res, next) {
+// function searchByStatus(status, oldArr, newArr) {
+//   for (var i = 0; i < oldArr.length; i++) {
+//     if (oldArr[i]["friendStatus"] == status) {
+//       newArr.push(oldArr[i]["friendID"]);
+//     }
+//   }
+//   return newArr;
+// }
 
-  res.json({ message: error.message});
+//Bella L created 31/08/19 - Api that displays all the user's pending friend requests
+router.route("/pendingRequests").get((req, res) => {
+  User.aggregate([
+      {
+        '$match': {
+          '_id': mongoose.Types.ObjectId('5d650a82d2c045ad621f44f4')
+        }
+      }, {
+        '$lookup': {
+          'from': 'users', 
+          'localField': 'USER_FRIENDS.friendID', 
+          'foreignField': '_id', 
+          'as': 'pendingrequests'
+        }
+      },
+    
+    { $project: { 
+      "_id": 0,
+      "friends": "$pendingrequests"
+      // "firstname": "$pendingrequests.firstname",
+      // "lastname": "$pendingrequests.lastname"
+    }}
+  ]).exec((err, friends) => {
+    if (err) console.log(err);
+    else res.json(friends);
+  });
+});
+
+ // {
+    //   "$match": { "_id": mongoose.Types.ObjectId(req.param.friendID) }
+    // },
+    // {
+    //   "$group": {
+    //     "_id": "$USER_FRIENDS.friendID",
+    //     "users": { "$addToSet": "$_id" }
+    //   }
+    // },
+    // {
+    //   "$match": {
+    //     "users": mongoose.Types.ObjectId(req.param.friendID) 
+    //   }
+    // }
+
+// {
+//   $lookup: {
+//     from: "users",
+//     let: {
+//       friendID: "$USER_FRIENDS.friendID",
+//       status: "$USER_FRIENDS.friendStatus"
+//     },
+//     pipeline: [
+//       {
+//         $match: {
+//           $expr: {
+//             $and: [
+//               { $eq: ["$friendID", "$_id"] },
+//               { $eq: ["$status", "Received"] }
+//             ]
+//           }
+//         }
+//       },
+//       { $project: { _id: 0}}
+//     ],
+//     as: "friendID"
+//   }
+// }
+
+//Use that userID to to find its JSON file in the DB
+
+//Finding user by their ID
+// User.findById("5d651255e0029ab11191e793", (err, user) => {
+//   if (err) console.log(err);
+//   else {
+//     res.json(
+//       user.aggregate([ //aggregate user object
+//         {
+//           $lookup: {
+//             from: "users", //lookup on users collection
+//             let: { friendID: "$USER_FRIENDS.friendID", status: "$USER_FRIENDS.friendStatus"}, //assigning friendID and status to their values
+//             pipeline: [
+//               {
+//                 $match: {
+//                   $expr: {
+//                     $and: [ //Where the WHERE starts
+//                       { $eq: ["$friendID", "$_id"] }, //friendIDs in user object equal to _id in the collection
+//                       { $eq: ["$status", "$Received"] } //status in user object equal to Received
+//                     ]
+//                   }
+//                 }
+//               },
+//               { $project: { _id: 0}}
+//             ],
+//             as: "friendID" //output array
+//           }
+//         }
+//       ])
+//     );
+//   }
+// });
+
+//Default Error-Handler:
+app.use(function(error, req, res, next) {
+  res.json({ message: error.message });
 });
 
 //creating a route for the backend that will pass through the json data for what you are querying
@@ -144,4 +251,3 @@ app.use("/", router);
 app.listen(port || 4000, () =>
   console.log("Express sever running on port " + port)
 );
-
