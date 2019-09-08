87bb09582d38f3533dbb38533d420e33611111c1
CD <lucy24928@gmail.com>
2019-09-08 15:23:11 +1000

    Finished create account + login

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -13,6 +13,33 @@ dotenv.config(); //allows us to use the .env file when developing code
 
 function routes(User) {
     const userRouter = express.Router();
+    userRouter.route("/user").get((req, res) => {
+      const query1 = {};
+      if (req.query.email && req.query.password) {
+        query1.email = req.query.email;
+        query1.password = req.query.password;
+      }
+      User.findOne(query1, (err, users) => {
+        if (err) {
+          console.dir("hellogcgfsd");
+          return res.status(404).send(err);
+        }
+        else if (users && users._id) {
+         console.log("eeeee");
+          var status = {
+            "UserID" : users._id
+          };
+          return res.status(200).send(status)
+        }
+        else {
+          console.log("rrrrrr");
+          var status = {
+            "Status" : "Incorrect email and/or password"
+          };
+          return res.status(200).send(status)
+        }
+      })
+    });
     userRouter.route("/user").post((req, res) => {
         const user = new User(req.body);
         console.dir(req.body.firstname);
@@ -48,14 +75,18 @@ function routes(User) {
               console.log(result);
             });
             var status = {
-              "Status" : "Account has been created"
+              "UserID" : user1._id
+              // "Status" : "Account has been created"
             };
             return res.status(201).send(status);
-            // return res.status(201).send("{Status': 'Account has been created'}");
-            // return res.status(201).send("{jdklfjs");
           }
         });
       });
+
+      
+
+
+
     return userRouter;
 }
 
